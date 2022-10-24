77b304a9f78da1961bf20f13d3ebea5b97809fc0
kcpele <fidekg123@gmail.com>
2022-10-24 05:31:41 +0100

    added user fetching

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,24 +1,26 @@
-import { lazy, useContext } from 'react'
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { ThemeContext } from "../utils/contexts/ThemeContext"
+import { lazy, useContext } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { ThemeContext } from "../utils/contexts/ThemeContext";
 
-const Navbar = lazy(() => import("./Navbar"))
-const Header = lazy(() => import("./Header"))
-const Main = lazy(() => import("./Main"))
+const Navbar = lazy(() => import("./Navbar"));
+const Header = lazy(() => import("./Header"));
+const Main = lazy(() => import("./Main"));
+const User = lazy(() => import("./Users"));
 
 const Layout = () => {
-    const { theme } = useContext(ThemeContext)
-    return (
-        <main className={`app ${theme}`}>
-            <Navbar />
-            <section className="header-main">
-                <ToastContainer className={`toast-container ${theme}`} />
-                <Header />
-                <Main />
-            </section>
-        </main>
-    )
-}
+  const { theme } = useContext(ThemeContext);
+  return (
+    <main className={`app ${theme}`}>
+      <Navbar />
+      <section className="header-main">
+        <ToastContainer className={`toast-container ${theme}`} />
+        <Header />
+        {/* <Main /> */}
+        <User />
+      </section>
+    </main>
+  );
+};
 
-export default Layout
+export default Layout;
